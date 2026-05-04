59c88834d242348d0096c54c857a83eb910f8e8d
goodmidnight <fancythat0831@gmail.com>
2026-05-04 18:14:30 +0900

    chore: add androidx documentfile dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.core.splashscreen)
+    implementation(libs.androidx.documentfile)
     implementation(libs.hilt.android)
     implementation(libs.androidx.datastore.preferences)
     implementation(libs.zxing.core)
